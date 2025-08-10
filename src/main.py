26071640e90b600a1a9fe01973b195d5b4c103fc
iannicolasdev <nicolasian002@gmail.com>
2025-08-10 19:33:42 -0300

    feat: add password saving to file database

--- src/main.py
+++ src/main.py
@@ -1,6 +1,27 @@
 import random 
 import string as st
+import json
+import os
 
 caracteres = st.ascii_letters + st.digits + st.punctuation
 
-print(''.join(random.choices(caracteres, k=12)))
+length = int(input("Qual o tamanho da senhas? "))
+
+password = (''.join(random.choices(caracteres, k=length)))
+
+if os.path.exists("data/password.json"):
+    try:
+        with open("data/password.json", "r") as file:
+            data = json.load(file)
+    except json.JSONDecodeError:
+        data = {"passwords": []}  
+else:
+    data = {"passwords": []}  
+
+data["passwords"].append(password)
+
+with open("data/password.json", "w") as file:
+    json.dump(data, file, indent=4)
+
+with open("data/password.json", "r") as file:
+    json.load(file)
