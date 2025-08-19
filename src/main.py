ed04114fb4cd7895317fbc295609feb81115aac0
iannicolasdev <nicolasian002@gmail.com>
2025-08-19 11:54:08 -0300

    feat: add initial SQLite database

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 import random 
 import string as st
+import sqlite3
 import json
 import os
 
@@ -9,19 +10,20 @@ length = int(input("Qual o tamanho da senhas? "))
 
 password = (''.join(random.choices(caracteres, k=length)))
 
-if os.path.exists("data/password.json"):
-    try:
-        with open("data/password.json", "r") as file:
-            data = json.load(file)
-    except json.JSONDecodeError:
-        data = {"passwords": []}  
-else:
-    data = {"passwords": []}  
+conn = sqlite3.connect("data/passwords.db")
+cursor = conn.cursor()
 
-data["passwords"].append(password)
+# if os.path.exists("data/password.json"):
+#     try:
+#         with open("data/password.json", "r") as file:
+#             data = json.load(file)
+#     except json.JSONDecodeError:
+#         data = {"passwords": []}  
+# else:
+#     data = {"passwords": []}  
 
-with open("data/password.json", "w") as file:
-    json.dump(data, file, indent=4)
+# data["passwords"].append(password)
 
-with open("data/password.json", "r") as file:
-    json.load(file)
+# with open("data/password.json", "w") as file:
+#     json.dump(data, file, indent=4)
+
