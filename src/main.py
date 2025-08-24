67d5bede67a1e4e7f74bfcc159abae71eb7d7912
iannicolasdev <nicolasian002@gmail.com>
2025-08-24 20:12:19 -0300

    feat: create initial table for password storage

--- src/main.py
+++ src/main.py
@@ -4,14 +4,23 @@ import sqlite3
 import json
 import os
 
-caracteres = st.ascii_letters + st.digits + st.punctuation
+# caracteres = st.ascii_letters + st.digits + st.punctuation
 
-length = int(input("Qual o tamanho da senhas? "))
+# length = int(input("Qual o tamanho da senhas? "))
 
-password = (''.join(random.choices(caracteres, k=length)))
+# password = (''.join(random.choices(caracteres, k=length)))
 
 conn = sqlite3.connect("data/passwords.db")
 cursor = conn.cursor()
+
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS passwords (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    service TEXT NOT NULL,
+    username TEXT,
+    password TEXT NOT NULL
+)
+""")
 
 # if os.path.exists("data/password.json"):
 #     try:
