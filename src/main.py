36e5ac0ed67e5876594c41783b35ebd434753521
iannicolasdev <nicolasian002@gmail.com>
2025-08-29 18:38:45 -0300

    refactor: remove old JSON-based password implementation

--- src/main.py
+++ src/main.py
@@ -28,18 +28,3 @@ VALUES ('GitHub', 'ian', '12345678');
 """)
 
 conn.commit()
-
-# if os.path.exists("data/password.json"):
-#     try:
-#         with open("data/password.json", "r") as file:
-#             data = json.load(file)
-#     except json.JSONDecodeError:
-#         data = {"passwords": []}  
-# else:
-#     data = {"passwords": []}  
-
-# data["passwords"].append(password)
-
-# with open("data/password.json", "w") as file:
-#     json.dump(data, file, indent=4)
-
