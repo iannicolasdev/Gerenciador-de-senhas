2b4e6eef1e0ac8fef040439a040718779bc4249b
iannicolasdev <nicolasian002@gmail.com>
2025-09-14 12:37:48 -0300

    refactor: extract function to display table in terminal

--- src/cli.py
+++ src/cli.py
@@ -3,3 +3,12 @@ def get_inputs():
     username = str(input("Qual o seu nome?: "))
     service = str(input("Qual o serviço da senha?: "))
     return username, service
+
+def list_table(resultado):
+    print(f"| {'ID':>3} | {'Service':<20} | {'User':<20} | {'Password':<20} |")
+    print("-" * 76)
+
+    for linha in resultado:
+        id_, service, user, password = linha
+
+        print(f"| {id_:>3} | {service:<20} | {user:<20} | {password:<20} |")
