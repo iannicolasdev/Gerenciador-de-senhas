5460fe1f8ca78fbbfd7fcf1e98b6f19c571b9cc1
iannicolasdev <nicolasian002@gmail.com>
2025-10-02 20:31:18 -0300

    fix: improve input validation and error handling for user prompts

--- src/cli.py
+++ src/cli.py
@@ -1,13 +1,14 @@
 # Entrada de dados do usuário
 def get_inputs():
     username = str(input("Informe o username: "))
-    service = str(input("Informe o serviço da senha: "))
 
     if username == "":
         print("Erro: O username não pode estar vazio")
         return None, None
+    
+    service = str(input("Informe o serviço da senha: "))
 
-    elif service == "":
+    if service == "":
         print("Erro: O serviço não pode estar vazio")
         return None, None
 
@@ -28,11 +29,24 @@ def list_table(resultado):
 def get_update_inputs(text_id, text):
     id_ = int(input(f"Informe o ID {text_id} que quer atualizar: "))
 
+    if id_ == "":
+        print("Erro: O ID não foi informado")
+        return None, None
+
     change = str(input(f"Informe {text}: "))
 
-    return id_, change
+    if change == "":
+        print("Erro: O a alteração não pode estar vazia")
+        return None, None
+
+    else:
+        return id_, change
 
 def get_delete_inputs():
     delete_id = int(input("Informe o ID da linha que quer excluir: "))
 
-    return delete_id
+    if delete_id == "":
+        print("Erro: O ID não foi informado")
+
+    else:
+        return delete_id
