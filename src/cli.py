950be18a9f2b359bee949c92f8a1cf7f40887f1a
iannicolasdev <nicolasian002@gmail.com>
2025-10-20 21:55:31 -0300

    feat: add input validation for empty fields in get_update_inputs

--- src/cli.py
+++ src/cli.py
@@ -28,21 +28,21 @@ def list_table(resultado):
 
 # Interação para receber as novas alterações
 def get_update_inputs(text_id, text):
-    id_ = int(input(f"Informe o ID {text_id} que quer atualizar: "))
 
-    if id_ == "":
-        print("Erro: O ID não foi informado")
-        return None, None
+    while True:
+        try:
+            id_ = int(input(f"Informe o ID {text_id} que quer atualizar: "))
+            validate_id = validate_inputs(text, str(id_))
 
-    change = str(input(f"Informe {text}: "))
+            change = str(input(f"Informe {text}: "))
+            validate_change = validate_inputs(text, change)
 
-    if change == "":
-        print("Erro: O a alteração não pode estar vazia")
-        return None, None
+            return int(validate_id), validate_change
+        
+        except ValueError:
+            print(f"Erro: Os campo não pode ser vazio.")
 
-    else:
-        return id_, change
-
+# Função para deletar dados no banco
 def get_delete_inputs():
     delete_id = int(input("Informe o ID da linha que quer excluir: "))
 
